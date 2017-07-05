be02fc80d8c3d63bd65c794f5c4f4512d294649f
Piotr Mazur <piotr.mazur.priv@gmail.com>
2017-07-05 19:11:51 +0200

    Doinstalowana paczka Jquery UI

--- PortalOgloszen/App_Start/BundleConfig.cs
+++ PortalOgloszen/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace PortalOgloszen
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,6 +26,7 @@ namespace PortalOgloszen
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/themes/base/jquery-ui.css",
                       "~/Content/site.css"));
         }
     }
